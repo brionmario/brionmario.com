0b7d8946911a686a13b944069e3c38bc27c3a53a
Brion Mario <info@brionmario.com>
2022-04-09 12:43:33 +0530

    chore(app): init app with `@geist-ui/core`

---
+++ apps/web/pages/_document.tsx
@@ -0,0 +1,34 @@
+import { CssBaseline } from "@geist-ui/core";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = CssBaseline.flush();
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      )
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,74 @@
-import { Button } from "ui";
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Brion Mario
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-export default function Web() {
+import { Button, Display, Grid, Image, Page, Text } from "@geist-ui/core";
+import Head from "next/head";
+
+const gh = "https://github.com/geist-org/geist-ui";
+const docs = "https://geist-ui.dev";
+
+export default function IndexPage() {
+  const redirect = (url: string) => window.open(url);
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <Head>
+        <title>Geist UI with NextJS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Page dotBackdrop width="800px" padding={0}>
+        <Display
+          title="Geist UI"
+          caption={
+            <>
+              Welcome to{" "}
+              <Text span b>
+                Geist UI
+              </Text>{" "}
+              and start learning more !
+            </>
+          }>
+          <Image src="/geist-banner.png" alt="geist ui banner" draggable={false} />
+        </Display>
+        <Grid.Container justify="center" gap={3} mt="100px">
+          <Grid xs={20} sm={7} justify="center">
+            <Button
+              shadow
+              type="secondary-light"
+              width="100%"
+              onClick={() => redirect(gh)}>
+              GitHub Repo
+            </Button>
+          </Grid>
+          <Grid xs={0} sm={3} />
+          <Grid xs={20} sm={7} justify="center">
+            <Button width="100%" onClick={() => redirect(docs)}>
+              Documentation Site
+            </Button>
+          </Grid>
+        </Grid.Container>
+      </Page>
     </div>
   );
 }
