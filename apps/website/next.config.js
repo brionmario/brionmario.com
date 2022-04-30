3d6f3dd1e3138bea6e8e4d7e35f630bdcfa3b39a
Brion Mario <info@brionmario.com>
2022-04-30 17:26:14 +0530

    ci(github): fix dev ci attempt 005

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -3,3 +3,18 @@ const withTM = require("next-transpile-modules")([ "@brionmario/ui" ]);
 module.exports = withTM({
   reactStrictMode: true
 });
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  webpack: (config) => {
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: require.resolve("react")
+        .replace("index.js", "")
+    };
+
+    return config;
+  }
+};
+
