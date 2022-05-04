a991423e6696b04e4288bd40320884f2add21b5d
Brion Mario <info@brionmario.com>
2022-05-04 15:38:39 +0530

    build: add maintainance mode .env var

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,5 +1,12 @@
 const withTM = require("next-transpile-modules")([ "@brionmario/ui" ]);
 
 module.exports = withTM({
-  reactStrictMode: true
+  reactStrictMode: true,
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "true"
+        ? { source: "/((?!coming-soon)(?!_next)(?!static)(?!assets).*)", destination: "/coming-soon", permanent: false }
+        : null
+    ].filter(Boolean);
+  }
 });
