8c625dfa4d6c9c8436c0ff22f5535f78d1142a5f
Brion <info@brionmario.com>
2023-08-12 19:44:41 +0530

    chore(apps): refactor `GitHubProjectIcon` component

---
+++ apps/website/components/icons/GithubProjectIcon.tsx
@@ -0,0 +1,58 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022, Brion Mario
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import cx from 'clsx';
+import {ReactElement, SVGProps} from 'react';
+
+/**
+ * `GitHubProjectIcon` is a React wrapper on GitHub's project icon SVG.
+ *
+ * Usage:
+ *
+ * ```jsx
+ * <GitHubProjectIcon className="github-icon" height={20} width={20} />;
+ * ```
+ *
+ * @param props - Props for the component.
+ * @returns GitHub project icon as a React Component.
+ */
+const GitHubProjectIcon = ({className, width = 16, height = 16, ...rest}: SVGProps<SVGSVGElement>): ReactElement => (
+  <svg
+    aria-hidden="true"
+    height={width}
+    viewBox={`0 0 ${height} ${width}`}
+    version="1.1"
+    width={width}
+    className={cx('bmui-icon', 'github-project-icon', 'svg', className)}
+    {...rest}
+  >
+    <path
+      fillRule="evenodd"
+      fill="#8b949e"
+      d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+    />
+  </svg>
+);
+
+export default GitHubProjectIcon;
